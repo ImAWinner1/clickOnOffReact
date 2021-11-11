568255e96365121006cc86fc244778957133091b
ghodrat keshtmand <60603913+ImAWinner1@users.noreply.github.com>
2021-11-11 20:50:41 +0330

    writeCode

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            value: "OFF"
+        };
+        this.togglevalue=this.togglevalue.bind(this)
+    }
+    togglevalue(){
+        this.setState((prevState)=>({
+            value: prevState.value === "OFF"? "ON": "OFF"
+
+        }));
+    }
+    render(){
+        return(
+            <button onClick={() => this.togglevalue()} >
+                {this.state.value}
+            </button>
+        )
+    }
+}
+ReactDom.render(
+    <App />,
+    document.getElementById("root")
+)
